bb87b8c652643078661e535dde95a075f6bd1b93
NHMETA <hungteestudio@gmail.com>
2026-05-16 05:13:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,12 @@ import re
 
 app = Flask(__name__, static_folder='.')
 
-ACCESS_TOKEN = "EAAVi5UFSu9cBRQMkbZBqo8ZBdAeOojyP7ZAS0qUtZCNnLE1rCqh5rdMKC5ZAWcSSEIOOYdyhZBWWSnI9MJNpDRkgwf6XNcm9GBzjxozeetYMIsYZAQqbSrJ2UGG6BPJ1n0PPMi0nHaGUdICd81wZBG5bHYMtRHnz3vrj9g3MiGMmHjJtFWHFm0Lv2oK5V3xQPxtANZCmnmDNhALkML43805uiHsnqY6H9tzJg4F8WW5hn9BApfILZAnzra5zwzygz20QetWZAHbmx42evoKCcZBX2eBLl8TA2mnDnuNsQ53CZA3xP0P0uMHhqLjZCk6XlXBUZBZBS510F8VL13LJApTNh4iT0BssxQHNGwZDZD"
 
 def check_page_has_likes(uid):
     url = f"https://graph.facebook.com/{uid}"
     try:
         res = requests.get(url, params={
-            "fields": "fan_count,name",
-            "access_token": ACCESS_TOKEN
+            "fields": "fan_count,name"
         }, timeout=10)
         data = res.json()
         if "error" in data:
